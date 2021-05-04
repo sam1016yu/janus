f3d652817423bace767f500ab729875d264154e2
sam1016yu <miaoyu1016@gmail.com>
2021-05-04 18:31:46 -0400

    updated scripts

--- test_run_more.sh
+++ test_run_more.sh
@@ -4,7 +4,7 @@ duration=30
 # prefix="test_run_all_others"
 # prefix="no_dist"
 # prefix="baseline"
-prefix="NP_"
+prefix="0503_10ms_large"
 
 set -v
 
@@ -117,6 +117,19 @@ function tpcc_all {
 	new_experiment $exp_name
 }
 
+function tpcc_all_large {
+	shards=$1
+	cpu=$2
+	concurrent=100
+	exp_name=${prefix}_tpccTJ2O_${shards}shards_${cpu}cpus_${concurrent}cc
+	write_concurrent $concurrent
+	# ./run_all.py -g -hh config/hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq  -m tapir:tapir -c 1 -c 10  -s $shards -u $cpu -r 3 -d $duration $exp_name
+	# ./run_all.py -g -hh config/hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq  -c 1 -c 2 -c 4 -c 8 -c 16 -c 24 -c 28 -c 32  -s $shards -u $cpu -r 3 -d $duration $exp_name
+	#  -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir
+	./run_all.py -g -hh config/hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq  -m tapir:tapir -m 2pl_ww:multi_paxos -m occ:multi_paxos -c 48 -c 64 -c 80 -c 96 -c 112 -c 128 -c 144 -c 160 -c 176 -c 192 -s $shards -u $cpu -r 3 -d $duration $exp_name
+	new_experiment $exp_name
+}
+
 function tpcc_NP_all {
 	shards=$1
 	cpu=$2
@@ -140,8 +153,8 @@ function tpcc_NP_all {
 
 
 function run_tests {
-	tpcc_NP_all 6 2
-	tpcc_NP_all 12 4
+	tpcc_all_large 6 2
+	tpcc_all_large 12 4
 	# tpcc_all 18 6
 	# tpcc_all 24 8
 	# tpcc_all 48 16
--- test_run_once.sh
+++ test_run_once.sh
@@ -28,7 +28,22 @@ function tpcc {
 	exp_name=${prefix}_tpcc_${shards}shards_${cpu}cpus_${concurrent}cc
 	# ./run_all.py -g -hh config/hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq  -m tapir:tapir -c 1 -c 10  -s $shards -u $cpu -r 3 -d $duration $exp_name
 	# ./run_all.py -g -hh config/hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq  -c 32  -s $shards -u $cpu -r 3 -d $duration $exp_name
-	./run_all.py -g -hh config/hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc2.yml -cc config/tapir.yml -b tpcc -m brq:brq   -c 32  -s $shards -u $cpu -r 3 -d $duration $exp_name
+	./run_all.py -g -hh config/hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq  -m tapir:tapir -m 2pl_ww:multi_paxos -m occ:multi_paxos  -c 200  -s $shards -u $cpu -r 3 -d $duration $exp_name
+	new_experiment $exp_name
+}
+
+
+
+function tpcc_all_large {
+	shards=$1
+	cpu=$2
+	concurrent=100
+	exp_name=${prefix}_tpccTJ2O_${shards}shards_${cpu}cpus_${concurrent}cc
+	write_concurrent $concurrent
+	# ./run_all.py -g -hh config/hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq  -m tapir:tapir -c 1 -c 10  -s $shards -u $cpu -r 3 -d $duration $exp_name
+	# ./run_all.py -g -hh config/hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq  -c 1 -c 2 -c 4 -c 8 -c 16 -c 24 -c 28 -c 32  -s $shards -u $cpu -r 3 -d $duration $exp_name
+	#  -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir
+	./run_all.py -g -hh config/hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc --m brq:brq  -m tapir:tapir -m 2pl_ww:multi_paxos -m occ:multi_paxos -c 196  -s $shards -u $cpu -r 3 -d $duration $exp_name
 	new_experiment $exp_name
 }
 
@@ -37,7 +52,7 @@ function run_tests {
 	# write_concurrent 100
 	# tpcc 18 6 100
 	write_concurrent 100
-	tpcc 6 2 100
+	tpcc_all_large 48 16
 	# write_concurrent 100
 	# tpcc 48 16 100
 
